e99529ba69c6d551286695169440a5ec01bbd527
DevMUA <joaomar2012@gmail.com>
2020-10-25 14:27:56 +0000

    first commit

---
+++ BehaviourTreeFramework/Selector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : Node
+{
+    protected List<Node> nodes = new List<Node>();
+    List<Node> removedNodes = new List<Node>();
+
+    public Selector(List<Node> nodes)
+    {
+        this.nodes = nodes;
+    }
+       public override NodeState Evaluate()
+    {
+        foreach(var node in nodes)
+        {
+            switch (node.Evaluate())
+            {
+                case NodeState.RUNNING:
+                    _nodeState = NodeState.RUNNING;
+                    return _nodeState;
+                case NodeState.SUCCESS:
+                    _nodeState = NodeState.SUCCESS;
+                    return _nodeState;
+                case NodeState.FAILURE:
+                default:
+                    break;
+            }
+        }
+        _nodeState = NodeState.FAILURE;
+        return _nodeState;
+    }
+/*
+    public void RemoveNodeFromList(int index)
+    {
+        removedNodes.Add(nodes[index]);
+        nodes.RemoveAt(index);
+    }
+
+    public void AddNodeToList(int index)
+    {
+        nodes.Insert(index, removedNodes[0]);
+        removedNodes.RemoveLast();
+    }*/
+}
---
+++ BehaviourTreeFramework/Sequence.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequence : Node
+{
+    protected List<Node> nodes = new List<Node>();
+
+    public Sequence(List<Node> nodes)
+    {
+        this.nodes = nodes;
+    }
+       public override NodeState Evaluate()
+    {
+        /*
+        bool isAnyNodeRunning = false;
+        foreach(var node in nodes)
+        {
+            switch (node.Evaluate())
+            {
+                case NodeState.RUNNING:
+                    isAnyNodeRunning = true;
+                    //return _nodeState = NodeState.RUNNING;
+                    break;
+                case NodeState.SUCCESS:
+                    break;
+                case NodeState.FAILURE:
+                    _nodeState = NodeState.FAILURE;
+                    return _nodeState;
+                default:
+                    break;
+            }
+        }
+        _nodeState = isAnyNodeRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+        return _nodeState;*/
+        
+        foreach (Node n in nodes)
+        {
+            switch (n.Evaluate())
+            {
+                case NodeState.RUNNING:
+                    return _nodeState = NodeState.RUNNING;
+                case NodeState.FAILURE:
+                    return _nodeState = NodeState.FAILURE;
+            }
+        }
+
+        return _nodeState = NodeState.SUCCESS;
+    }
+}
